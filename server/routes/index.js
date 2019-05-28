23a8975b25624e98d588152a7552cd294225a783
Soo Kim <chizbob@gmail.com>
2019-05-28 06:29:21 -0700

    layout error fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,34 +1,48 @@
-const express = require("express");
+const express = require("express")
 
-const router = express.Router();
-
-const speakersRoute = require("./speakers")
-const feedbackRoute = require("./feedback")
+const router = express.Router()
 
 module.exports = (param) => {
 
   const { speakerService } = param
 
-  router.get("/", async (req, res, next) => {
+  router.get('/', async (req, res, next) => {
     try {
       const promises = []
-      promises.push(speakerService.getListShort())
+      promises.push(speakerService.getList())
       promises.push(speakerService.getAllArtwork())
 
       const results = await Promise.all(promises)
 
-      return res.render("index", {
-        page: "Home",
-        speakerslist: results[0],
-        artwork: results[0]
+      return res.render('speakers', {
+          page: 'All Speakers',
+          speakerslist: results[0],
+          artwork: results[1],
       })
-      } catch(err) {
-          return next(err)
-      }
+    } catch(err) {
+        return err
+    }
   })
 
-  router.use("/speakers", speakersRoute(param))
-  router.use("/feedback", feedbackRoute(param))
+  router.get('/:name', async (req, res, next) => {
+    try {
+      const promises = []
+      promises.push(speakerService.getSpeaker(req.params.name))
+      promises.push(speakerService.getArtworkForSpeaker(req.params.name))
+      const results = await Promise.all(promises)
+
+      if(!results[0]) {
+          return next()
+      }
+
+      return res.render('speakers/detail', {
+          page: req.params.name,
+          speaker: results[0],
+          artwork: results[1],
+      })
+    } catch(err) {
+        return next(err)
+    }
+  })
 
   return router
-}
